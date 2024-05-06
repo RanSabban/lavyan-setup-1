92152d39a9c7c65431b7906b570424fa5ec11d09
Ran Sabban <sabbanran@gmail.com>
2024-05-06 14:10:55 +0300

    First init

---
+++ src/app/ui/signup-form.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { signup } from "@/app/lib/actions/auth";
+import { useFormState, useFormStatus } from "react-dom";
+
+export function SignupForm() {
+
+    const [state, action] = useFormState(signup, undefined)
+
+    return (
+        <form action={action}>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input id="name" name="name" placeholder="Name" />
+            </div>
+            {state?.errors?.name && <p>{state.errors.name}</p>}
+            <div>
+                <label htmlFor="email">Email</label>
+                <input id="email" name="email" type="email" placeholder="Email" />
+            </div>
+            {state?.errors?.email && <p>{state.errors.email}</p>}
+            <div>
+                <label htmlFor="password">Password</label>
+                <input id="password" name="password" type="password" />
+            </div>
+            {state?.errors?.password && (
+                <div>
+                    <p>Password must:</p>
+                    <ul>
+                        {state.errors.password.map((error) => (
+                            <li key={error}>- {error}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            <SignupButton />
+        </form>
+    )
+}
+
+export function SignupButton() {
+    const { pending } = useFormStatus()
+
+    return (
+        <button aria-disabled={pending} type="submit">
+            {pending ? 'Signing up...' : 'Sign Up'}
+        </button>
+    )
+}
