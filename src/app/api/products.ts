24f9ccc454ba33b5f608f2d61689652c393dc372
Ran Sabban <sabbanran@gmail.com>
2024-05-08 17:21:19 +0300

    Remove product api for prod

--- src/app/api/products.ts
+++ src/app/api/products.ts
@@ -9,36 +9,36 @@ type Data = {
   error?: string;
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  switch (req.method) {
-    case 'GET':
-      try {
-        const products = await prisma.product.findMany();
-        res.status(200).json({ products });
-      } catch (error: any) {
-        res.status(500).json({ error: error.message });
-      }
-      break;
-    case 'POST':
-      try {
-        const { name, email, password } = req.body
-        const newUser = await prisma.product.create({
-          data: {
-            name,
-            email,
-            password
-          },
-        });
-        res.status(201).json({ products: [newUser] });
-      } catch (error: any) {
-        res.status(500).json({ error: error.message });
-      }
-      break;
-    default:
-      res.setHeader('Allow', ['GET', 'POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<Data>
+// ) {
+//   switch (req.method) {
+//     case 'GET':
+//       try {
+//         const products = await prisma.product.findMany();
+//         res.status(200).json({ products });
+//       } catch (error: any) {
+//         res.status(500).json({ error: error.message });
+//       }
+//       break;
+//     case 'POST':
+//       try {
+//         const { name, email, password } = req.body
+//         const newUser = await prisma.product.create({
+//           data: {
+//             name,
+//             email,
+//             password
+//           },
+//         });
+//         res.status(201).json({ products: [newUser] });
+//       } catch (error: any) {
+//         res.status(500).json({ error: error.message });
+//       }
+//       break;
+//     default:
+//       res.setHeader('Allow', ['GET', 'POST']);
+//       res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+// }
